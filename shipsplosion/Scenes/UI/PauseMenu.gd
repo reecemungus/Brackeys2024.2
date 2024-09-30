92dd592e5bf59d67161ebde0b6ee6540c4abbe41
Harrison Marden-Evans (s5522805) <s5522805@bournemouth.ac.uk>
2024-09-30 12:55:27 +0100

    Starting on pause menu

---
+++ shipsplosion/Scenes/UI/PauseMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var pause_menu = $"."
+var paused = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
